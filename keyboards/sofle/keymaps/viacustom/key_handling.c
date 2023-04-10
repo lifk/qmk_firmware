b33473b8dae11299dd704b9be01526e3e7aa286c
Luis Jurado <luisbcr@gmail.com>
2023-04-10 16:03:29 +0200

    add back is calm

--- keyboards/sofle/keymaps/viacustom/key_handling.c
+++ keyboards/sofle/keymaps/viacustom/key_handling.c
@@ -2,6 +2,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case KC_LCTL:
   case KC_RCTL:
+#ifdef OCEAN_DREAM_ENABLE
+    is_calm = (record->event.pressed) ? true : false;
+#endif;
     if (record->event.pressed) {
       isSneaking = true;
     } else {
